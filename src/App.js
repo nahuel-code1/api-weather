32e657def5190ce5bc8065e86fd7fe0cd80b27f6
Nahuel Mereles <nahuelmereles1@gmail.com>
2021-05-13 02:05:29 -0400

    create state hooks for the data

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import './App.css'
 export default function App() {
     let [latitude, setLatitude] = useState(0);
     let [longitude, setLongitude] = useState(0);
-    let [weatherdata, setweatherdata] = useState();
+    let [name, setName] = useState("");
+    let [country, setCountry] = useState("");
     let [temp, setTemp] = useState(0);
+    let [urlIcon, setUrlIcon] = useState("");
+    let [description, setDescription] = useState("");
+    let [clouds, setClouds] = useState(0);
+    let [windspeed, setWinspeed] = useState(0);
+    let [humidity, setHumidity] = useState(0);
+    let [pressure, setPressure] = useState(0);
     let [toggle, setToggle] = useState(true);
     let [units, setUnits] = useState("C°");
 
@@ -20,13 +27,19 @@ export default function App() {
                 setLongitude(dataLongitude);
             });
             const url = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=7711b0bc3dff50cf8d0839629484fd23&units=metric`);
-            setweatherdata(url.data);
+            setName(url.data.name);
+            setCountry(url.data.sys.country);
             setTemp(url.data.main.temp);
+            setUrlIcon("http://openweathermap.org/img/w/" + url.data.weather[0].icon + ".png");
+            setDescription(url.data.weather[0].description)
+            setClouds(url.data.clouds.all);
+            setWinspeed(url.data.wind.speed);
+            setHumidity(url.data.main.humidity);
+            setPressure(url.data.main.pressure);
         }
         fetchApi();
     }, [latitude, longitude])
-    
-    let iconUrl = "http://openweathermap.org/img/w/" + weatherdata.weather[0].icon + ".png";
+
 
     const handleCtoF = () => {
         let changeTemp = (temp * 1.8)  + 32;
@@ -42,17 +55,18 @@ export default function App() {
         setUnits(changeUnit)
     }
 
+ 
     
 
     return ( 
         <div className="background"> 
             <div className="weather-box">
                 <h1 className="text-center">WEATHER APP</h1>
-                <h3 className="text-center">{weatherdata.name}, {weatherdata.sys.country}</h3>
+                <h3 className="text-center">{name}, {country}</h3>
                 <div className="weather-container">
                     <div className="icon-image">
                         <div className="icon-image_2">
-                            <img src={iconUrl} alt="icon weather"/>
+                            <img src={urlIcon} alt="icon weather"/>
                         </div>
                         <div>
                             <h3 className="temp">{temp} {units}</h3>
@@ -60,11 +74,11 @@ export default function App() {
                     </div>
                     <div className="info-weather">  
                         <div className="info-weather_2">
-                            <h3 className="mt-3">"{weatherdata.weather[0].description}"</h3>
-                            <h3 className="mt-3">Clouds: {weatherdata.clouds.all}%</h3>
-                            <h3 className="mt-3">Wind Speed: {weatherdata.wind.speed} m/s</h3>
-                            <h3 className="mt-3">Humedity: {weatherdata.main.humidity}%</h3>
-                            <h3 className="mt-3">Pressure: {weatherdata.main.pressure} hPa</h3>
+                            <h3 className="mt-3">"{description}"</h3>
+                            <h3 className="mt-3">Clouds: {clouds}%</h3>
+                            <h3 className="mt-3">Wind Speed: {windspeed} m/s</h3>
+                            <h3 className="mt-3">Humedity: {humidity}%</h3>
+                            <h3 className="mt-3">Pressure: {pressure} hPa</h3>
                         </div>
                     </div>
                 </div>
